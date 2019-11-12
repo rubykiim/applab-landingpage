9773d63a091769fd1ca044aaeff89eea6728b783
Ruby <IKID@gimseung-aui-MacBook-Pro.local>
2019-11-12 20:54:44 +0800

    ver_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img
+        className="image"
+        src="https://images.pexels.com/photos/886521/pexels-photo-886521.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+      ></img>
+
+      <div className="logo">
+        <p className="EcoBox">EcoBox</p>
+      </div>
+      
+      <div className="nav_bar">
+        <h3>Contact Us</h3>
+      </div>
+
+      <div className="slogan_1">
+        <p>Love to shop online,</p>
+        <p>but still want to be eco-friendly?</p>
+      </div>
+
+      <div className="slogan_2">
+        <p> <span>EcoBox</span>, your best choice for reusable packaging</p>
+      </div>
+      <Input></Input>
     </div>
+  );
+}
+
+function Input() {
+  return (
+    <input placeholder=" Enter your email to learn more" type="text" />
+  );
+}
+
+function Button() {
+  return (
+    <button></button>
   );
 }
 
